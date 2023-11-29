e617a3a5735f84c08c83ca79224a7addf92a3c90
Bato <bato19941994@bk.ru>
2023-11-29 15:07:42 +0800

    addError

--- src/test/java/PostmanTest.java
+++ src/test/java/PostmanTest.java
@@ -22,7 +22,7 @@ public class PostmanTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some value"));
+                .body("data", equalTo("some valu"));
     }
 
     }
